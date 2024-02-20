70f6a1a9e87c54ee5200751a16575d6e87f83c29
Chandan Manna <chandan.unitedmonks@gmail.com>
2024-02-20 19:14:08 +0530

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics,GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Quick Note",
   description: "just write & share",
+  
 };
 
 export default function RootLayout({
@@ -17,6 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleTagManager gaId="G-DR4ZJ9ZSQ8" />
+      <GoogleAnalytics gaId="G-DR4ZJ9ZSQ8" />
+
     </html>
   );
 }
